b87412e253c373f1dbf75dc6c640a425cf9c0b0d
Sergeevu4 <sergei.sergeevu4@gmail.com>
2020-03-07 16:47:21 +0300

    swapi-service-contex: Уточняющие комментарии

--- src/components/swapi-service-contex/swapi-service-contex.js
+++ src/components/swapi-service-contex/swapi-service-contex.js
@@ -1,6 +1,20 @@
 import React from 'react';
 
-// ! Создания React Context
+/*
+  ! Создания React Context
+     ? Используется: Смена темы оформления, смена языка
+
+    React Context - необязательно должен быть статичным,
+    значение в контексте можно обновлять как любое другое свойство компонента
+
+    Например: Смена сервиса данных в App
+    Если Context обновился, обновятся все дочерние компоненты.
+
+    Если требуется обновить свойства:
+    componentDidUpdate(prevProps) - функция в которой можно проверить, какие свойства изменились
+    this.props.getData !== prevProps.getData
+*/
+
 const {
   Provider: SwapiServiceProvider, // Переименовываю
   Consumer: SwapiServiceConsumer,
