a8476a613c64def2303ba15c83adaa6e3c079b24
Sergeevu4 <sergei.sergeevu4@gmail.com>
2020-03-07 16:46:44 +0300

    with-child-function: Новое отображения HOC в Developer Tools

--- src/components/hoc-helpers/with-child-function.js
+++ src/components/hoc-helpers/with-child-function.js
@@ -3,10 +3,27 @@ import React from 'react';
 // # Компонент высшего порядка
 // # Паттерн React: Частичное примененные функции
 // * Функция возвращает компонент ItemList c функцией Children внутри
-const withChildFunction = (fn) => (Wrapped) =>
+const withChildFunction = (fn) => (Wrapped) => {
   function WithChildFunction(props) {
     // props принимает от withData -> itemList
     return <Wrapped {...props}>{fn}</Wrapped>;
-  };
+  }
+
+  // ! Необходимо для более удобного отображения HOC внутри React Developer Tools
+  WithChildFunction.displayName = `withChildFunction(${Wrapped.displayName ||
+    Wrapped.name}`;
+  return WithChildFunction;
+};
 
 export default withChildFunction;
+
+/*
+  # Стандартный способ отображения HOC в React Developer
+  const withChildFunction = (fn) => (Wrapped) =>
+    function WithChildFunction(props) {
+      // props принимает от withData -> itemList
+      return <Wrapped {...props}>{fn}</Wrapped>;
+    };
+
+  export default withChildFunction;
+*/
