6b09457b81bc6e050124a24fbb8125b62d3c253d
Sergeevu4 <sergei.sergeevu4@gmail.com>
2020-03-27 15:21:53 +0300

    Updates

---
+++ precache-manifest.b90d98566e3d637e6e2285e0a6ac723e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19f6e91be5d969e7829bdb3bd85769f1",
+    "url": "/Star-Wars-React/index.html"
+  },
+  {
+    "revision": "bf05a596378a850467cb",
+    "url": "/Star-Wars-React/static/css/main.47875cc0.chunk.css"
+  },
+  {
+    "revision": "c2550e4fe07fc945aaa0",
+    "url": "/Star-Wars-React/static/js/2.6102eec5.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/Star-Wars-React/static/js/2.6102eec5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "bf05a596378a850467cb",
+    "url": "/Star-Wars-React/static/js/main.4cc0a8d0.chunk.js"
+  },
+  {
+    "revision": "66bdc1595fe4431559db",
+    "url": "/Star-Wars-React/static/js/runtime-main.1cfc00ea.js"
+  }
+]);
